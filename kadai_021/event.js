8d5cc1f2f85214cc5dfa968336083925027bcd6a
ogasoma <154291837+ogasoma@users.noreply.github.com>
2024-01-25 10:01:01 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,19 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+
+
+
+ // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+ setTimeout(() => {
+  text.textContent = "ボタンをクリックしました";
+  console.log('処理4（非同期処理）');
+}, 2000);
+
+
+});
